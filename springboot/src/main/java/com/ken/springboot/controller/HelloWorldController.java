51a91f22fd6c68406d2d4f29878f4a70597af7c2
kenshirokk <iamseptemdash@hotmail.com>
2021-08-09 23:25:29 +0800

    ADD SWAGGER

--- springboot/src/main/java/com/ken/springboot/controller/HelloWorldController.java
+++ springboot/src/main/java/com/ken/springboot/controller/HelloWorldController.java
@@ -1,14 +1,19 @@
 package com.ken.springboot.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/helloworld")
+@Api(tags = "1.0.0-SNAPSHOT", description = "X", value = "X")
 public class HelloWorldController {
 
     @GetMapping
+    @ApiOperation(value = "x", notes = "x")
+//    @ApiImplicitParams({@ApiImplicitParam(name = "username", value = "用户名", dataType = DataType.STRING, paramType = ParamType.QUERY, defaultValue = "xxx")})
     public String helloWorld() {
         return "hello world";
     }
