a38a767363b04e1690119c05816e02c8a74beb47
kenshirokk <iamseptemdash@hotmail.com>
2021-08-17 18:38:43 +0800

    n

---
+++ Loki/src/test/java/com/ken/loki/LokiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ken.loki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LokiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
