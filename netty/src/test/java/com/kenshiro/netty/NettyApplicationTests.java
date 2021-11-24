78e72975db46ee9d17892aec9b7f50be19b2f87d
kenshirokk <iamseptemdash@hotmail.com>
2021-11-24 22:24:30 +0800

    n

---
+++ netty/src/test/java/com/kenshiro/netty/NettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kenshiro.netty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
