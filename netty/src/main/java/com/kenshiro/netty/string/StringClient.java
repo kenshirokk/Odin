3acc7d2f4859ddd73ec152d61084beb97a1550ee
kenshirokk <iamseptemdash@hotmail.com>
2021-11-28 21:58:23 +0800

    netty

---
+++ netty/src/main/java/com/kenshiro/netty/string/StringClient.java
@@ -0,0 +1,26 @@
+package com.kenshiro.netty.string;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.string.StringEncoder;
+
+public class StringClient {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Bootstrap b = new Bootstrap();
+        b.group(new NioEventLoopGroup())
+                .channel(NioSocketChannel.class)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringEncoder());
+                    }
+                });
+        ChannelFuture cf = b.connect("127.0.0.1", 8080);
+        cf.sync().channel().writeAndFlush("helloworld");
+    }
+}
---
+++ netty/src/main/java/com/kenshiro/netty/string/StringServer.java
@@ -0,0 +1,57 @@
+package com.kenshiro.netty.string;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public class StringServer {
+    public static void main(String[] args) {
+        ServerBootstrap b = new ServerBootstrap();
+        EventLoopGroup boss = new NioEventLoopGroup(1);
+        EventLoopGroup worker = new NioEventLoopGroup();
+
+        b.group(boss, worker)
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.WARN))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new StringDecoder());
+                        ch.pipeline().addLast("handler1", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                System.out.println("===================handler1");
+                                System.out.println(msg);
+//                                ctx.fireChannelRead(msg);
+                                ctx.fireChannelActive();
+                            }
+
+                            @Override
+                            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                System.out.println("handler1 active");
+                            }
+                        }).addLast("handler2", new ChannelInboundHandlerAdapter() {
+                            @Override
+                            public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+                                System.out.println("===================handler2");
+                                System.out.println(msg);
+                            }
+
+                            @Override
+                            public void channelActive(ChannelHandlerContext ctx) throws Exception {
+                                System.out.println("handler2 active");
+                            }
+                        });
+                    }
+                });
+        b.bind(8080);
+    }
+}
