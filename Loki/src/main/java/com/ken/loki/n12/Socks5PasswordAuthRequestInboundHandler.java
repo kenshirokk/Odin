542d136825c86afee29424b06c441667c3bd215c
kenshirokk <iamseptemdash@hotmail.com>
2023-07-02 19:07:04 +0800

    2

---
+++ Loki/src/main/java/com/ken/loki/n12/Socks5PasswordAuthRequestInboundHandler.java
@@ -0,0 +1,35 @@
+package com.kdyzm.trojan.client.netty.inbound;
+
+import com.kdyzm.trojan.client.netty.properties.ConfigUtil;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.socksx.v5.*;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author kdyzm
+ * @date 2021/4/25
+ */
+@Slf4j
+@AllArgsConstructor
+public class Socks5PasswordAuthRequestInboundHandler extends SimpleChannelInboundHandler<DefaultSocks5PasswordAuthRequest> {
+
+    private final ConfigUtil configUtil;
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, DefaultSocks5PasswordAuthRequest msg) throws Exception {
+        //认证成功
+        if (configUtil.getUsers().get(msg.username()).equals(msg.password().trim())) {
+            Socks5PasswordAuthResponse passwordAuthResponse = new DefaultSocks5PasswordAuthResponse(Socks5PasswordAuthStatus.SUCCESS);
+            ctx.writeAndFlush(passwordAuthResponse);
+            ctx.pipeline().remove(this);
+            ctx.pipeline().remove(Socks5PasswordAuthRequestDecoder.class);
+            return;
+        }
+        Socks5PasswordAuthResponse passwordAuthResponse = new DefaultSocks5PasswordAuthResponse(Socks5PasswordAuthStatus.FAILURE);
+        //发送鉴权失败消息，完成后关闭channel
+        ctx.writeAndFlush(passwordAuthResponse).addListener(ChannelFutureListener.CLOSE);
+    }
+}
